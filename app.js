08d2a6ee053825910860d2b1640f6a50afd67e52
1nsomniac <samthakala@gmail.com>
2016-11-10 14:14:06 -0700

    Just the paint mini-challenge in class. I got it to (kinda) work with click and drag. would like to revisit the property/logic set later for a deeper understanding.

---
+++ app.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  var color
+  // $('.box').on('click', function() {
+  //   $(this).toggleClass(color);
+  // })
+  $('.box').on('mousedown', function() {
+    $('.box').on('mouseover', function() {
+      $(this).toggleClass(color);
+    })
+    $('.box').on('mouseup', function() {
+      $('.box').off('mouseover')
+    })
+  })
+  $('#reset').on('click', function() {
+    $('.box').removeClass()
+  })
+  $('#red').on('click', function() {
+    color = 'red';
+  })
+  $('#blue').on('click', function() {
+    color = 'blue';
+  })
+  $('#green').on('click', function() {
+    color = 'green';
+  })
+  $('#yellow').on('click', function() {
+    color = 'yellow';
+  })
+  $('#white').on('click',  function() {
+    color = 'white';
+  })
+})
