7aabdef130c46049e932aaea81f38f0b5b8cbf6e
1nsomniac <samthakala@gmail.com>
2016-11-10 14:18:22 -0700

    minor linter changes :)

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-$(document).ready(function() {
-  var color
-  // $('.box').on('click', function() {
+$(document).ready(function () {
+  var color;
+  // $('.box').on('click', function () {
   //   $(this).toggleClass(color);
   // })
-  $('.box').on('mousedown', function() {
-    $('.box').on('mouseover', function() {
+  $('.box').on('mousedown', function () {
+    $('.box').on('mouseover', function () {
       $(this).toggleClass(color);
     })
-    $('.box').on('mouseup', function() {
-      $('.box').off('mouseover')
+    $('.box').on('mouseup', function () {
+      $('.box').off('mouseover');
     })
   })
-  $('#reset').on('click', function() {
-    $('.box').removeClass()
+  $('#reset').on('click', function () {
+    $('.box').removeClass();
   })
-  $('#red').on('click', function() {
+  $('#red').on('click', function () {
     color = 'red';
   })
-  $('#blue').on('click', function() {
+  $('#blue').on('click', function () {
     color = 'blue';
   })
-  $('#green').on('click', function() {
+  $('#green').on('click', function () {
     color = 'green';
   })
-  $('#yellow').on('click', function() {
+  $('#yellow').on('click', function () {
     color = 'yellow';
   })
-  $('#white').on('click',  function() {
+  $('#white').on('click', function () {
     color = 'white';
   })
 })
